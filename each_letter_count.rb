eb13cec2bea1f08dc23c34206b807248d6613ba0
svenka8 <sriram.venkat@chicagobooth.edu>
2020-10-10 21:51:11 +0000

    updated

--- each_letter_count.rb
+++ each_letter_count.rb
@@ -4,3 +4,13 @@
 #  The program should print each letter in the word the number of times it appears in the word.
 
 p "Enter a word:"
+#word = gets.chomp.split("")
+word = ["a", "s", "d", "f", "a", "d", "a", "s", "d", "f"]
+#unique= word.uniq
+#p unique
+#unique
+word.each do |letter| 
+#p letter
+#p word.at(3) # this works to print f which is at position 3 (0,1,2,3) 10 times - which is the letter  count
+#p index
+end
